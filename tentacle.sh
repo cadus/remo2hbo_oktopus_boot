bbb2adb3055b59c3d663c4ff6a0b0aacffaef680
Paul Hänsch <paul@plutz.net>
2018-11-15 16:01:10 +0100

    serve tentacles as HTTP

--- tentacle.sh
+++ tentacle.sh
@@ -2,4 +2,17 @@
 
 field="$1"
 
+CR="$(printf \\r)"
+
+read METHOD P PROTOCOL
+while read name value; do
+  [ ! "${name%$CR}" ] && break
+done
+
+printf 'HTTP 200 OK\r\n'
+printf '%s: %s\r\n' \
+  Connection close \
+  Content-Type text/plain
+printf '\r\n'
+
 ncat localhost 8200 |cut -d\  -f${field}
