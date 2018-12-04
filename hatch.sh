e8df6b9db3c4307deecf3dcde7354da6ea7f827d
Paul Hänsch <paul@plutz.net>
2018-12-04 14:56:06 +0100

    mind connection limit of ncat

--- hatch.sh
+++ hatch.sh
@@ -6,9 +6,9 @@ rm "$sock"
 
 ( cd "${0%/*}/"
   ./gummikraken.sh ./gummikraken.data \
-  | ncat --send-only -klU "$sock"
+  | ncat -m 1000 --send-only -klU "$sock"
 ) &
 
 for n in 1 2 3 4 5 6 7 8 9 0; do [ ! -S "$sock" ] && sleep 1; done
 chmod a+rw "$sock"
-ncat -U "$sock" >/dev/null &
+# ncat -U "$sock" >/dev/null &
