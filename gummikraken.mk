16dbafa8e746545eb45bd3a873de6322e2be8675
Paul Hänsch <paul@plutz.net>
2019-01-02 11:27:51 +0100

    temporarily fix hardcoded ip in interface submodule

--- gummikraken.mk
+++ gummikraken.mk
@@ -3,5 +3,6 @@ files/srv/gummikraken/: gummikraken/ sensors/ .FORCE
 	cp -av "$</." sensors/. "$@/."
 	chmod a+rX -R "$@"
 	cp gummikraken/inetd.conf files/etc/inetd.conf
+	sed -ri 's;http://[0-9\.]+:8200;http://oktopus:8200;g' "$@/oktopus_frontend/dist/bundle.js"
 
 raspi.img: files/srv/gummikraken/
