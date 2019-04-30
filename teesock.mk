91bde30b457a727d36c530923ce1c75d11f2e343
Paul Hänsch <paul@plutz.net>
2019-04-30 16:25:11 +0200

    copy teesock.arm to file name teesock

--- teesock.mk
+++ teesock.mk
@@ -4,7 +4,7 @@ ${IMGFILE}: teesock
 teesock: imgmount root_copy teesock/teesock.arm
 	mkdir -p $</usr/local/bin
 	chmod 755 $</usr/ $</usr/local/ $</usr/local/bin/
-	cp -av teesock/teesock.arm "$</usr/local/bin/"
+	cp -av teesock/teesock.arm "$</usr/local/bin/teesock"
 
 teesock/teesock.arm: teesock/teesock.c teesock/Makefile
 	make -C "$(dir $@)" "$(notdir $@)"
